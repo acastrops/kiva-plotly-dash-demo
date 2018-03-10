41c660d791e281c3e4b3910a5bc39b514779dc16
mikejt33 <mikejt33@gmail.com>
2018-03-10 14:45:11 -0500

    Completing Map code - exists as a working app

--- cloroplethmap.py
+++ cloroplethmap.py
@@ -13,8 +13,6 @@ df['date'] = pd.to_datetime(df['date'])
 df['year'] = df.date.dt.year
 df['year'] = df.year.astype(str).astype(int)
 
-#df = df.groupby(['year', 'country','borrower_genders']).size()
-
 df = df[['country','borrower_genders','year']]
 df.dropna()
 df[['borrower_genders']].astype(str)
@@ -28,33 +26,29 @@ df = df.loc[mask]
 df['borrower_genders'].replace('female',1, inplace=True)
 df['borrower_genders'].replace('male',0, inplace=True)
 
-#countries_funded_amount = df.loc[df['year'] == 2014]
-
 
 app = dash.Dash()
 
 app.layout = html.Div(className='container', children=[
         
-    # Richard's cloropleth map
+    # Geographical Map
     html.Div([
         html.Hr(),
         html.H1(
         children='Cloropleth maps including slider percent colormap',
         style={
-            'textAlign': 'center',  # center the header
+            'textAlign': 'center', 
             'color': '#7F7F7F'
-            # https://www.biotechnologyforums.com/thread-7742.html more color code options
         }
     ),
         dcc.Graph(id='graph-with-slider'),
-        html.Div([  # div inside div for style
+        html.Div([  
             dcc.Slider(
                 id='year-slider',
                 min=df['year'].min(),
                 max=df['year'].max(),
-                value=df['year'].min(),  # The default value of the slider
+                value=df['year'].min(),
                 step=None,
-                # the values have to be the same dtype for filtering to work later
                 marks={str(year): year for year in df['year'].unique()},
             )
         ],
@@ -72,33 +66,16 @@ app.layout = html.Div(className='container', children=[
     )
 ])
 ])
-        
-        
-# Notice the Input and Outputs in this wrapper correspond to
-# the ids of the components in app.layout above.
+
+
 @app.callback(
     Output('graph-with-slider', 'figure'),
     [Input('year-slider', 'value')])
 def update_figure(selected_year):
     """Define how the graph is to be updated based on the slider."""
 
-    # Depending on the year selected on the slider, filter the db
-    # by that year.
-
-    # snag: using .groupby() with more than one feature caused the datatype
-    # to be Pandas.Series instead of Pandas.DataFrame. So, we couldn't just do
-    # countries_funded_amount[countries_funded_amount['year'] == selected_year]
-    
-        
-    #one_year_data = df.loc[df['year'] == [selected_year]]
-    #df = df.loc[df['year'] == [selected_year]]
     df1 = df.loc[df['year'] == [selected_year]]
 
-    
-    #df = countries_funded_amount.loc[selected_year]
-    #df = countries_funded_amount.loc[2017]
-    
-    ## Counts number of times a country appears
     a = df1['country'].value_counts()
 
 ## gets sum related to country
@@ -121,39 +98,23 @@ def update_figure(selected_year):
     ticksmax = percent[0].max()
     ticks = np.linspace(ticksmin, ticksmax, 8)
     label = ticks.astype(np.int)
-    
-    #one_year_data = countries_funded_amount.loc[selected_year]
 
 
     data = [dict(
         type='choropleth',
         locations= percent['country'],
-        # list of country names
-        # other option is USA-states
         locationmode='country names',
-        # sets the color values. using log scale so that extreme values don't
-        # drown out the rest of the data
-        z= percent[0].astype(float),# ...and their associated values
-        # sets the text element associated w each position
+        z= percent[0].astype(float),
         text=percent['country'],
-        hoverinfo='location+z',  # hide the log-transformed data values
-        # other colorscales are available here:
-        # https://plot.ly/ipython-notebooks/color-scales/
+        hoverinfo='location+z',
         colorscale='Blues',
-        # by default, low numbers are dark and high numbers are white
         reversescale=True,
-        # set upper bound of color domain (see also zmin)
-        # zmin=200,
-        # zmax=30000,
-        # if you want to use zmin or zmax don't forget to disable zauto
-        # zauto=False,
-        marker={'line': {'width': 0.5}},  # width of country boundaries
+        marker={'line': {'width': 0.5}},  
         colorbar={'autotick': True,
-                  'tickprefix': '%',  # could be useful if plotting $ values
+                  'tickprefix': '%',  
                   'title': 'Percent of loans Female',
                   'tickvals': ticks,
-                  'ticktext': label# colorbar title
-                  # transform log label labels back to standard scale
+                  'ticktext': label
                   },
     )]
     layout = dict(
@@ -161,7 +122,7 @@ def update_figure(selected_year):
                 <a href="https://www.kaggle.com/kiva/data-science-for-good-kiva-crowdfunding"">\
                 Kaggle</a>'.format(selected_year),
         font=dict(family='Courier New, monospace', size=18, color='#7f7f7f'),
-        geo={'showframe': False}  # hide frame around map
+        geo={'showframe': False}  
     )
     cloropleth_map_fig = {'data': data, 'layout': layout}
     return cloropleth_map_fig
